6323df61215a427cddb141e776dc300a7bae231e
Santanu <santunuachary@gmail.com>
2019-11-03 17:23:37 +0530

    I am Team Lead Created A Project So all the Team Members will be implement the remaining Code and send me for a review after completion. This is my First Commit.

---
+++ src/com/automation/Class1.java
@@ -0,0 +1,11 @@
+package com.automation;
+
+public class Class1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Class1 created by TeamLeader");
+
+	}
+
+}
